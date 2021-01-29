9833f1c96b4b2561bc4799d3b41cb9b469ba53fe
Israel Lima <israel.lima@ceuma.br>
2021-01-29 17:00:41 -0300

    style: create routes and add new favicon

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+
+
+const routes = [
+  { path: '/', redirectTo: '/home', pathMatch: 'full' },
+  { path: '/home', component: HomeComponent },
+  { path: '/map', component: MapComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
