7a5f20f220fb407d23199a6c7078422997721d17
Israel Lima <israel.vitor.dev@gmail.com>
2021-01-30 12:23:47 -0300

    chore: add firebase project configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAU82OturDTvvunZrDDZTqUza_KCTIKs5k",
+    authDomain: "for-storage-65cd6.firebaseapp.com",
+    projectId: "for-storage-65cd6",
+    storageBucket: "for-storage-65cd6.appspot.com",
+    messagingSenderId: "258044185514",
+    appId: "1:258044185514:web:81d1b1efb7b5871227b9d3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAU82OturDTvvunZrDDZTqUza_KCTIKs5k",
+    authDomain: "for-storage-65cd6.firebaseapp.com",
+    projectId: "for-storage-65cd6",
+    storageBucket: "for-storage-65cd6.appspot.com",
+    messagingSenderId: "258044185514",
+    appId: "1:258044185514:web:81d1b1efb7b5871227b9d3"
+  }
 };
 
 /*
