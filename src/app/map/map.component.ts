fde5a9a9648c7c68d0d2b9ee4fb2631551f5cabb
Israel Lima <israel.vitor.dev@gmail.com>
2021-01-30 16:29:44 -0300

    feat: add map page

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { SuccessComponent } from '../dialogs/success/success.component';
+import { TipComponent } from '../dialogs/tip/tip.component';
+import { CoreService } from '../services/core.service';
+
+export interface tipDialogData {
+  position: string
+}
 
 @Component({
   selector: 'app-map',
@@ -7,9 +16,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private coreService: CoreService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    // this.coreService.readAnswers().subscribe((answers) => {
+    //   if (answers.length === 5) {
+    //     const dialogRef = this.dialog.open(SuccessComponent, {
+    //       width: '700px'
+    //     })
+
+    //     dialogRef.afterClosed().subscribe((result) => {
+    //       this.router.navigate(['home'])
+    //     });
+    //   }
+    // })
+  }
+
+  public openTipDialog(position: string): void {
+    this.dialog.open(TipComponent, {
+      width: '700px',
+      data: { position: position }
+    });
   }
 
 }
