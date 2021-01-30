5f109e6eb0b00f69aca404e9f83ff1d0335cec1d
Israel Lima <israel.vitor.dev@gmail.com>
2021-01-30 16:27:49 -0300

    build: add angular material and remove bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,44 @@ import { HomeComponent } from './home/home.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TipComponent } from './dialogs/tip/tip.component';
+import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import { AdminComponent } from './admin/admin.component';
+import { SuccessComponent } from './dialogs/success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     HomeComponent,
+    TipComponent,
+    AdminComponent,
+    SuccessComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
-  providers: [],
+  entryComponents: [TipComponent],
+  providers: [
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
