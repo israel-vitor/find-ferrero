9cac822878a8b8c4ac5dd22f86a02a75b9f4f799
Israel Lima <israel.vitor.dev@gmail.com>
2021-01-30 16:30:19 -0300

    feat: add read and create functions

--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface answer {
+  id?: string;
+  answer: string;
+  created_at: any;
+}
+
+export interface mapAccess {
+  id?: string;
+  accessed_at: any;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +20,41 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class CoreService {
 
   private mapAccessCollectionName = "map-access"
+  private answersCollectionName = "answers"
   constructor(private angularFirestore: AngularFirestore) { }
 
   public createMapAccessRegister(): Promise<any> {
     return this.angularFirestore.collection(this.mapAccessCollectionName).add({
-      "accessed-at": Date.now()
+      "accessed_at": new Date()
     })
   }
+
+  public createAnswer(answer: string): Promise<any> {
+    return this.angularFirestore.collection(this.answersCollectionName).add({
+      "created_at": new Date(),
+      "answer": answer
+    })
+  }
+
+  public readAnswers(): Observable<answer[]> {
+    return this.angularFirestore.collection<answer>(this.answersCollectionName).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as answer;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  public readMapAccess(): Observable<mapAccess[]> {
+    return this.angularFirestore.collection<mapAccess>(this.mapAccessCollectionName).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as mapAccess;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
 
 }
